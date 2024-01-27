a8eb0a2f89e1a4c7b09fc35f5a691ae5f4099c62
Subin43 <subin44christal@gmail.com>
2024-01-27 08:43:39 +0530

    contact page

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+
+import ContactUs from '../contact/contact';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <ContactUs />
+    </div>
+  );
+}
+
+export default App;
